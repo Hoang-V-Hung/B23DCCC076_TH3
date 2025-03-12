6ef7dcee687fce362782052fadc9c42c4444f928
Hoang-V-Hung <164860937+Hoang-V-Hung@users.noreply.github.com>
2025-03-12 11:51:45 +0700

    bài thực hành 3

--- config/routes.ts
+++ config/routes.ts
@@ -43,71 +43,6 @@ export default [
 		icon: 'ArrowsAltOutlined',
 	},
 	{
-		path: '/todolist',
-		name: 'Todolist',
-		component: './Todolist',
-		icon: 'FormOutlined',
-	},
-	{
-		path: '/game',
-		name: 'Game',
-		component: './Game',
-		icon: 'PlayCircleOutlined',
-	},
-	{
-		path: '/app',
-		name: 'App',
-		icon: 'AppstoreOutlined',
-		routes: [
-			{
-				path: 'quanlydanhmucmonhoc',
-				name: 'Quản lý danh mục môn học',
-				// exact: true,
-				component: './App/quanlydanhmucmonhoc/quanlydanhmucmonhoc',
-			},
-			{
-				path: 'quanlytiendohoctap',
-				name: 'Quản lý tiến độ học tập',
-				// exact: true,
-				component: './App/quanlytiendohoctap/quanlytiendohoctap',
-			},
-			{
-				path: 'thietlapmuctieuhoctap',
-				name: 'Thiết lập mục tiêu học tập',
-				// exact: true,
-				component: './App/thietlapmuctieuhoctap/thietlapmuctieuhoctap',
-			},
-		],
-	},
-	{
-		path: '/rps',
-		name: 'Rock Paper Scissors',
-		component: './RockPaperScissors',
-		icon:'RocketOutlined'
-	},
-	{
-		path: '/quanlycauhoi',
-		name: 'Quản lý Câu Hỏi Tự Luận',
-		Icon: 'QuestionCircleOutlined',
-		routes: [
-			{
-				path: 'quanlymonhoc',
-				name: 'Quản lý môn học',
-				component:'./QuanLyCauHoiTuLuan/QuanLyMonHoc/QuanLyMonHoc',
-			},
-			{
-				path: 'quanlycauhoi',
-				name: 'Quản lý câu hỏi',
-				component: './QuanLyCauHoiTuLuan/QuanLyCauHoi/QuanLyCauHoi',
-			},
-			{
-				path: 'quanlydethi',
-				name: 'Quản lý đề thi',
-				component: './QuanLyCauHoiTuLuan/QuanLyDeThi/QuanLyDeThi',
-			},
-		],
-	},
-	{
 		path: '/appoint-mate',
 		name: 'AppointMate',
 		routes: [
@@ -138,7 +73,6 @@ export default [
 			}
 		],
 		icon:'CarryOutOutlined'
-		// icon: 'ArrowsAltOutlined',
 	},
 
 	// DANH MUC HE THONG
